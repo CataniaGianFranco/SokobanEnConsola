09015d1b58130d15da2ce3967b68fa7a9150a86a
Chichodel1995 <>
2020-06-06 20:26:03 -0300

    Se agregaron Clases
    Clase Nivel
    Clase Pared
    Clase Piso

---
+++ Sokoban-en-consola/Nivel.cs
@@ -0,0 +1,15 @@
+﻿using Sokoban_en_consola.Elementos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban_en_consola
+{
+    class Nivel
+    {
+        Pared[,] matrizPared = new Pared[8,8];
+        Piso[,] matrizPiso = new Piso[8, 8];
+    }
+}
