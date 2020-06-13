3923201184ff9feec67b836d4ff947f92698c701
Chichodel1995 <>
2020-06-13 20:12:33 -0300

    Se mejoro el movimiento del personaje y ahora puede caminar si hay pso o no

--- Sokoban-en-consola/Escenario_01.cs
+++ Sokoban-en-consola/Escenario_01.cs
@@ -12,6 +12,7 @@ namespace Sokoban_en_consola
     {
         GameObject[,] gameObject = new GameObject[4, 4];
         GameObject jugador = null;
+        Posicion posicionamiento = null;
         ConsoleKeyInfo tecla = new ConsoleKeyInfo();
         private void Inicio()
         {
@@ -24,7 +25,7 @@ namespace Sokoban_en_consola
         public void Actualizar()
         {
             Inicio();
-            MoverJugador();
+            LeerInput();
         }
         private void CargarGameObjects()
         {
@@ -55,42 +56,45 @@ namespace Sokoban_en_consola
                 Console.WriteLine();
             }
         }
-        private void MoverJugador()
+        private void LeerInput()
         {
             do
             {
                 tecla = Console.ReadKey();
-                if (tecla.Key == ConsoleKey.RightArrow)
+                switch (tecla.Key)
                 {
-                    Console.Clear();
-                    jugador.posicion = new Posicion(jugador.posicion.Fila, jugador.posicion.Columna + 1);
-                    gameObject[jugador.posicion.Fila, jugador.posicion.Columna] = jugador;
-                    gameObject[jugador.posicion.Fila, jugador.posicion.Columna - 1] = new Piso();
+                    case ConsoleKey.RightArrow:
+                        posicionamiento = new Posicion(0, 1);      
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        posicionamiento = new Posicion(0, -1);
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        posicionamiento = new Posicion(-1, 0);
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        posicionamiento = new Posicion(1, 0);
+                        break;
+
+                    default:
+                         throw new Exception();
                 }
-                if (tecla.Key == ConsoleKey.LeftArrow)
-                {
-                    Console.Clear();
-                    jugador.posicion = new Posicion(jugador.posicion.Fila, jugador.posicion.Columna -1);
-                    gameObject[jugador.posicion.Fila, jugador.posicion.Columna] = jugador;
-                    gameObject[jugador.posicion.Fila, jugador.posicion.Columna + 1] = new Piso();
-                }
-                if (tecla.Key == ConsoleKey.UpArrow)
-                {
-                    Console.Clear();
-                    jugador.posicion = new Posicion(jugador.posicion.Fila - 1, jugador.posicion.Columna);
-                    gameObject[jugador.posicion.Fila, jugador.posicion.Columna] = jugador;
-                    gameObject[jugador.posicion.Fila + 1, jugador.posicion.Columna] = new Piso();
-                }
-                if (tecla.Key == ConsoleKey.DownArrow)
-                {
-                    Console.Clear();
-                    jugador.posicion = new Posicion(jugador.posicion.Fila + 1, jugador.posicion.Columna);
-                    gameObject[jugador.posicion.Fila, jugador.posicion.Columna] = jugador;
-                    gameObject[jugador.posicion.Fila - 1, jugador.posicion.Columna] = new Piso();
-                }
+                Console.Clear();
+                ActualizarJugador(posicionamiento.Fila,posicionamiento.Columna);
                 MostrarEscenario();
-            } while (tecla.Key != ConsoleKey.Enter);
-            
+            } while (tecla.Key != ConsoleKey.Enter);   
+        }
+        private void ActualizarJugador(int pFila, int pColumna)
+        {
+            if (jugador.PuedoCaminar() == gameObject[jugador.posicion.Fila + pFila, jugador.posicion.Columna + pColumna].PuedoCaminar())
+            {
+                jugador.posicion = new Posicion(jugador.posicion.Fila + pFila, jugador.posicion.Columna + pColumna);
+                gameObject[jugador.posicion.Fila, jugador.posicion.Columna] = jugador;
+                gameObject[jugador.posicion.Fila - pFila, jugador.posicion.Columna - pColumna] = new Piso();
+            }
             
         }
     }
